ec7fe81f783ff0e128d256914f5e58c360ed68ab
Yanni Brahmi <yanni914@hotmail.com>
2022-02-04 17:04:08 +0100

    second commit, install need

--- routes/routes.js
+++ routes/routes.js
@@ -5,4 +5,11 @@ import HomeController from "../controllers/home";
 
 router.get("/", HomeController );
 
+router.post('/', upload.single('photo'), (req, res) => {
+    console.log(req.file);
+    const { name, age, description } = req.body;
+    const kitty = new Kitten({ name: name, age: age, description: description });
+    kitty.save().then(() => console.log('meow'));
+    res.status(200).send("Envoyer");
+})
 export default router;
